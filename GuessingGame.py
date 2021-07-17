89d2edf592477b14a88c31d9162e92573ae93500
riteshtajrajchavhan <74711100+riteshtajrajchavhan@users.noreply.github.com>
2021-07-17 12:07:28 +0530

    Add files via upload

---
+++ GuessingGame.py
@@ -0,0 +1,12 @@
+import random
+number = random.randint(1,9)
+guess = int(input("guess a number between 1-10: "))
+print(number)
+if (guess==number):
+    print("hurray you won!!")
+elif(guess==number-5):
+    print("very close")
+elif(guess==number+5):    
+    print("very close")
+else:
+    print("better luck next time")
